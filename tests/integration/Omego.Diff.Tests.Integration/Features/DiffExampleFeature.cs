c558c6317ba93a7d3fc61993710859201e4055d2
Oleg Kleyman <Omego2K@gmail.com>
2015-04-12 13:21:04 -0400

    added scenario to test example

--- tests/integration/Omego.Diff.Tests.Integration/Features/DiffExampleFeature.cs
+++ tests/integration/Omego.Diff.Tests.Integration/Features/DiffExampleFeature.cs
@@ -1,6 +1,15 @@
-﻿namespace Omego.Diff.Tests.Integration.Features
+﻿using Xbehave;
+
+namespace Omego.Diff.Tests.Integration.Features
 {
     public class DiffExampleFeature
     {
+        [Scenario()]
+        public void GenerateDiffStats()
+        {
+            "Given I have git diff file".f(() => { }).Skip("Step to be implemented");
+            "When I run Omego.Diff".f(() => { }).Skip("Step to be implemented");
+            "Then I should se the git diff stats".f(() => { }).Skip("Step to be implemented");
+        }
     }
 }

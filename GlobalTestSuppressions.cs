8c9f21cdc056e7eb52fb6b87666290f53cada7f1
Oleg Kleyman <Omego2K@gmail.com>
2015-04-17 16:40:10 -0400

    added test suppressions

---
+++ GlobalTestSuppressions.cs
@@ -0,0 +1,6 @@
+﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters",
+    MessageId = "Xbehave.StringExtensions.f(System.String,System.Action)", 
+    Scope = "member",
+    Target = "Omego.Diff.Tests.Integration.Features.DiffExampleFeature.#GenerateDiffStats()",
+    Justification = "Not needed in test code")]

b0856b3eca36ae6728bdc4c6fc8b4836b8c07b28
Oleg Kleyman <Omego2K@gmail.com>
2015-04-22 17:21:29 -0400

    removed unused constructor

--- src/Omego.Diff/GitDiffReader.cs
+++ src/Omego.Diff/GitDiffReader.cs
@@ -4,11 +4,6 @@ namespace Omego.Diff
 {
     public class GitDiffReader : DiffReader
     {
-        public GitDiffReader(string diff)
-        {
-            throw new System.NotImplementedException();
-        }
-        
         public override IEnumerable<DiffRecord> ReadAll()
         {
             throw new System.NotImplementedException();

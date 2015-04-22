7604f552ca6e9582847f74144183bd70775e29dc
Oleg Kleyman <Omego2K@gmail.com>
2015-04-22 16:44:49 -0400

    renamed test class to a better suited name

--- tests/unit/Omego.Diff.Tests.Unit/DiffResultTests.cs
+++ tests/unit/Omego.Diff.Tests.Unit/DiffResultTests.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 namespace Omego.Diff.Tests.Unit
 {
     [TestFixture]
-    public class DiffTests
+    public class DiffResultTests
     {
         private readonly Mock<DiffReader> mockDiffReader = new Mock<DiffReader>();
 

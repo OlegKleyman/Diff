17be1960502e480890ca9728fa0a3396820c7322
Oleg Kleyman <Omego2K@gmail.com>
2015-04-22 17:43:01 -0400

    added constructor for argument

--- src/Omego.Diff/GitDiffReader.cs
+++ src/Omego.Diff/GitDiffReader.cs
@@ -4,6 +4,11 @@ namespace Omego.Diff
 {
     public class GitDiffReader : DiffReader
     {
+        public GitDiffReader(string diff)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public override IEnumerable<DiffRecord> ReadAll()
         {
             throw new System.NotImplementedException();
